9cf077a4d7bf547ff143fbea3a3ad3333c6d69d2
FabioClem <fabio.henrique.clem@gmail.com>
2017-02-06 10:04:48 -0200

    Adicionado mensagem de validação para tipo atividade.

--- suporten2/src/vraptor_suporten2/controller/routes/AtendimentoController.java
+++ suporten2/src/vraptor_suporten2/controller/routes/AtendimentoController.java
@@ -21,52 +21,51 @@ import vraptor_suporten2.model.entities.Atendimento;
 
 @Controller
 @RequestScoped
-public class AtendimentoController  extends AbstractCrudController{
-
-	@Inject
-	private AtendimentoDAO dao;
-
-	@Inject
-	private RedeDAO redeDao;
-
-	@Inject
-	private SessionUsuarioEfika session;
-
-	@Inject
-	private Atendimento atn;
-
-	public AtendimentoController() {
-
-	}
-
-	public AtendimentoController(Atendimento atn) {
-		this.atn = atn;
-	}
-
-	@Admin
-	public List<Atendimento> list() {
-		return dao.listar();
-	}
-
-	@Logado
-	@Path("/atendimento/")
-	public void create() {
-		result.include("atendimento", this.atn);
-		result.include("redeList", redeDao.listar());
-	}
-
-
-	@Logado
-	public void add(Atendimento a) {
-
-		a.setLoginRegistro(session.getUsuario().getLogin());
-		a.setDataRegistro(new Date());
-
-		this.atn = a;
-
-		if(a.getLoginOperador() == null){
-			validation.add(new SimpleMessage("a.loginOperador", "Campo requerido!"));
-		}else{
+public class AtendimentoController extends AbstractCrudController {
+    
+    @Inject
+    private AtendimentoDAO dao;
+    
+    @Inject
+    private RedeDAO redeDao;
+    
+    @Inject
+    private SessionUsuarioEfika session;
+    
+    @Inject
+    private Atendimento atn;
+    
+    public AtendimentoController() {
+        
+    }
+    
+    public AtendimentoController(Atendimento atn) {
+        this.atn = atn;
+    }
+    
+    @Admin
+    public List<Atendimento> list() {
+        return dao.listar();
+    }
+    
+    @Logado
+    @Path("/atendimento/")
+    public void create() {
+        result.include("atendimento", this.atn);
+        result.include("redeList", redeDao.listar());
+    }
+    
+    @Logado
+    public void add(Atendimento a) {
+        
+        a.setLoginRegistro(session.getUsuario().getLogin());
+        a.setDataRegistro(new Date());
+        
+        this.atn = a;
+        
+        if (a.getLoginOperador() == null) {
+            validation.add(new SimpleMessage("a.loginOperador", "Campo requerido!"));
+        } else {
 
 //			Pattern pattern;
 //			Matcher matcher;
@@ -77,27 +76,30 @@ public class AtendimentoController  extends AbstractCrudController{
 //			if(!matcher.matches()){
 //				validation.add(new SimpleMessage("a.loginOperador", "Utilize o formato: G0042204/A0042204!"));
 //			}
-			
-		}
-
-		if(a.getTerminal() == null){
-			validation.add(new SimpleMessage("a.terminal", "Campo requerido!"));
-		}			
-
-		if(a.getSolucao().getId() == null){
-			validation.add(new SimpleMessage("a.solucao.id", "Campo requerido!"));
-		}	
-
-		validation.onErrorForwardTo(AtendimentoController.class).create();
-
-		try {
-			dao.cadastrar(a);
-			this.atn = null;
-			result.include("mensagem", a.getClass().getSimpleName() + " cadastrado com sucesso!");
-			result.use(Results.logic()).redirectTo(this.getClass()).create();
-		} catch (Exception e) {
-			result.include("mensagemFalha", "Falha ao cadastrar " + a.getClass().getSimpleName() + "!");
-			result.use(Results.logic()).redirectTo(this.getClass()).create();
-		}
-	}
+        }
+        
+        if (a.getTipoAtividade() == null) {            
+            validation.add(new SimpleMessage("a.tipoAtividade", "Campo requerido!"));            
+        }
+        
+        if (a.getTerminal() == null) {
+            validation.add(new SimpleMessage("a.terminal", "Campo requerido!"));
+        }        
+        
+        if (a.getSolucao().getId() == null) {
+            validation.add(new SimpleMessage("a.solucao.id", "Campo requerido!"));
+        }        
+        
+        validation.onErrorForwardTo(AtendimentoController.class).create();
+        
+        try {
+            dao.cadastrar(a);
+            this.atn = null;
+            result.include("mensagem", a.getClass().getSimpleName() + " cadastrado com sucesso!");
+            result.use(Results.logic()).redirectTo(this.getClass()).create();
+        } catch (Exception e) {
+            result.include("mensagemFalha", "Falha ao cadastrar " + a.getClass().getSimpleName() + "!");
+            result.use(Results.logic()).redirectTo(this.getClass()).create();
+        }
+    }
 }
